bac9c445c64d3fb930c5cfa7a62b574b11cd50e4
yassinessadi <yassinessadi>
2023-12-22 19:39:45 +0000

    clean the code and handle some error with token

--- app/receiver-databricks.py
+++ app/receiver-databricks.py
@@ -1,10 +1,25 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC import `libs` & `models`:
+# MAGIC
+
+# COMMAND ----------
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, split, expr,concat_ws,regexp_extract,regexp_replace,when
 from pyspark.sql.functions import col, unbase64,date_format
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC `variables` & `expressions`
+
+# COMMAND ----------
+
 
 namespace = "regulargazelleseventhub"
 accessKeyName = "RootManageSharedAccessKey"
@@ -14,35 +29,60 @@ starting_position = -1
 
 connection_string = f"Endpoint=sb://{namespace}.servicebus.windows.net/;SharedAccessKeyName={accessKeyName};SharedAccessKey={accessKey};EntityPath={eventHubName}"
 
+#-------------------------------------#
+# event hub config to read more check #
+#--------------------------------------#
+
+
+#------------------------------------------------------------------------------------------------------------#
+# https://github.com/Azure/azure-event-hubs-spark/blob/master/docs/PySpark/structured-streaming-pyspark.md   #
+#------------------------------------------------------------------------------------------------------------#
+
 ehConf = {
     "eventhubs.consumerGroup": "$Default",
-    #"eventhubs.startingPosition": starting_position
 }
+#---------------------------------------------------------------------------------------------------------#
+# For 2.3.15 version and above, the configuration dictionary requires that connection string be encrypted.# 
+#---------------------------------------------------------------------------------------------------------#
 
 ehConf['eventhubs.connectionString'] = sc._jvm.org.apache.spark.eventhubs.EventHubsUtils.encrypt(connection_string)
+#--------------------#
+# init spark session #
+#--------------------#
 
 spark = SparkSession.builder.appName("EventHubslogshandler").getOrCreate()
 
-# Read from Event Hubs
+# COMMAND ----------
+
+
+#----------------------#
+# Read from Event Hubs #
+#----------------------#
 raw_df = spark \
   .readStream \
   .format("eventhubs") \
   .options(**ehConf) \
   .load()
 
-# Convert the value column from binary to string
+#------------------------------------------------#
+# Convert the value column from binary to string #
+#------------------------------------------------#
 raw_df = raw_df.withColumn("value", raw_df["body"].cast("string"))
 
-# Split the data using the "|" delimiter
+#----------------------------------------#
+# Split the data using the "|" delimiter #
+#----------------------------------------#
+
 split_columns = split(raw_df["value"], "\\|")
 split_df = raw_df.withColumn("split_data", split_columns)
-
-# Select the split columns and apply the defined schema
+#---------------------------------------------------------#
+# Select the split columns and apply the defined schema   #
+#---------------------------------------------------------#
 parsed_df = split_df.selectExpr("split_data[0] as timestamp", "split_data[1] as log_level", "split_data[2] as request_id", "split_data[3] as session_id", "split_data[4] as user_id", "split_data[5] as action", "split_data[6] as http_method", "split_data[7] as url", "split_data[8] as referrer_url", "split_data[9] as ip_address", "split_data[10] as user_agent", "split_data[11] as response_time", "split_data[12] as product_id", "split_data[13] as cart_size", "split_data[14] as checkout_status", "split_data[15] as token", "split_data[16] as auth_method", "split_data[17] as auth_level", "split_data[18] as correlation_id", "split_data[19] as server_ip", "split_data[20] as port", "split_data[21] as protocol", "split_data[22] as status", "split_data[23] as detail")
 
-# ------------------------
-# realtime transformation
-#-------------------------
+# ------------------------#
+# realtime transformation #
+#-------------------------#
 
 pattern = r'(\d+\.\d+)'
 
@@ -51,9 +91,8 @@ select_trasnformed_df = parsed_df.select(
     split(col("ip_address"), ":").getItem(1).alias("ip_address"),
     split(col("server_ip"), ":").getItem(1).alias("server_ip"),
     col("session_id").alias("session_id"),
-    col("url").alias("url"),
+    regexp_replace(col("url").alias("url"),"/","").alias("page_name"),
     split(col("Referrer_url"), "\\.").getItem(1).alias("Referrer_url"),
-    # col("Referrer_url").alias("Referrer_url"),
     split(col("cart_size"), ":").getItem(1).alias("cart_size"),
     split(col("checkout_status"), ":").getItem(1).alias("checkout_status"),
     split(col("product_id"), ":").getItem(1).alias("product_id"),
@@ -72,16 +111,26 @@ select_trasnformed_df = parsed_df.select(
     col("action").alias("action"),
 )
 
+
 select_trasnformed_df = select_trasnformed_df.withColumn("Referrer_url", \
     when(col("Referrer_url").isNull(),"Direct Entry") \
     .otherwise(col("Referrer_url"))) \
 
+#-------------------------------------------------------------------#
+# remove the data contains a null value (timestamp & response_time) #
+#-------------------------------------------------------------------#
 select_trasnformed_df = select_trasnformed_df.filter("timestamp IS NOT NULL and response_time IS NOT NULL")
 
-# Write the parsed DataFrame to the console sink
-select_trasnformed_df.writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .start()
+#------------------------------------------------#
+# Write the parsed DataFrame to the console sink #
+#------------------------------------------------#
 
+# select_trasnformed_df.writeStream \
+#     .outputMode("append") \
+#     .format("console") \
+#     .start()
+
+#--------------------#
+# display the output #
+#--------------------#
 display(select_trasnformed_df)
